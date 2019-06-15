9670f995eb992ae96a7f5e9755511f6906e43118
TomaEduard <eduard.daniel.toma@gmail.com>
2019-06-15 03:56:32 +0300

    Relation Link->Comments & abstract class Auditable extending Link & Comments

--- src/main/java/com/example/springit/domain/Link.java
+++ src/main/java/com/example/springit/domain/Link.java
@@ -8,11 +8,14 @@ import lombok.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Link {
+public class Link extends Auditable{
 
     @Id
     @GeneratedValue
@@ -22,7 +25,9 @@ public class Link {
     @NonNull
     private String url;
 
-    // comments
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
+
 
 
 }

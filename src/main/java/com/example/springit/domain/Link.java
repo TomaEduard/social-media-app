1868038a9f4425cff11230688f4e149fc3951c33
TomaEduard <eduard.daniel.toma@gmail.com>
2019-06-15 03:22:04 +0300

    Initial Repository for Link & Comment

--- src/main/java/com/example/springit/domain/Link.java
+++ src/main/java/com/example/springit/domain/Link.java
@@ -3,6 +3,7 @@ package com.example.springit.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,8 +17,9 @@ public class Link {
     @Id
     @GeneratedValue
     private Long id;
-
+    @NonNull
     private String title;
+    @NonNull
     private String url;
 
     // comments
